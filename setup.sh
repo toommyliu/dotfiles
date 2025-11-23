52186cf6e1108198d87bdd7a32135a8b19617621
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-23 12:02:47 -0800

    feat: initial setup script

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "Starting setup..."
+
+# install rosetta on apple silicon
+if [[ "$(sysctl -n machdep.cpu.brand_string)" == *'Apple'* ]]; then
+	if [ ! -d "/usr/libexec/rosetta" ]; then
+		echo "Installing Rosetta..."
+		sudo softwareupdate --install-rosetta --agree-to-license
+	fi
+	
+	# verify install
+	sudo softwareupdate --history
+fi
+
+# install xcode cli tools
+command -v "xcode-select -p" >/dev/null 2>&1; has_xcode=1 || { has_xcode=0; }
+if [ "$has_xcode" -eq 0 ]; then
+  echo "Installing XCode CLI Tools..."
+  sudo xcode-select --install
+else
+  # verify install
+  xcode-select -p
+  xcode-select --version
+fi
+
+# install homebrew
+command -v brew >/dev/null 2>&1; has_brew=1 || { has_brew=0; }
+if [ "$has_brew" -eq 0 ]; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # add 'brew --prefix' location to $PATH
+  # https://applehelpwriter.com/2018/03/21/how-homebrew-invites-users-to-get-pwned/
+  # https://www.n00py.io/2016/10/privilege-escalation-on-os-x-without-exploits/
+  if [[ "$(sysctl -n machdep.cpu.brand_string)" == *'Apple'* ]]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.bash_profile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    
+    #echo 'export PATH=/opt/homebrew/bin:$PATH' >> /Users/${USER}/.bash_profile
+    #echo 'export PATH=/opt/homebrew/sbin:$PATH' >> /Users/${USER}/.bash_profile
+  else
+    echo 'export PATH="/usr/local/sbin:$PATH"' >> /Users/${USER}/.bash_profile
+  fi
+
+  source /Users/${USER}/.bash_profile
+
+  # turn off brew analytics
+  brew analytics off
+if
