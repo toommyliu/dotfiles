e62bd42d64be04d693ff25ca96bc4a1e036f537b
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-25 08:20:59 -0800

    setup: improve xcode-cli detection

--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@ if [[ "$(sysctl -n machdep.cpu.brand_string)" == *'Apple'* ]]; then
 		echo "Installing Rosetta..."
 		sudo softwareupdate --install-rosetta --agree-to-license
 	fi
-	
+
 	# verify install
 	sudo softwareupdate --history
 fi
 
 # install xcode cli tools
-command -v "xcode-select -p" >/dev/null 2>&1; has_xcode=1 || { has_xcode=0; }
-if [ "$has_xcode" -eq 0 ]; then
+if ! xcode-select -p &>/dev/null; then
   echo "Installing XCode CLI Tools..."
-  sudo xcode-select --install
+  xcode-select --install
+  echo "Please complete the XCode CLI Tools installation in the dialog, then re-run this script."
+  exit 0
 else
-  # verify install
-  xcode-select -p
+  echo "XCode CLI Tools already installed at: $(xcode-select -p)"
   xcode-select --version
 fi
 
@@ -37,7 +37,7 @@ if [ "$has_brew" -eq 0 ]; then
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.zprofile
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.bash_profile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    
+
     #echo 'export PATH=/opt/homebrew/bin:$PATH' >> /Users/${USER}/.bash_profile
     #echo 'export PATH=/opt/homebrew/sbin:$PATH' >> /Users/${USER}/.bash_profile
   else
@@ -59,19 +59,19 @@ if [ -d "$HOME/.nvm" ]; then
   echo "Setting up NVM and installing Node.js versions..."
   export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
-  
+
   # install latest LTS version
   echo "Installing Node.js LTS..."
   nvm install --lts
-  
+
   # install latest version
   echo "Installing latest Node.js..."
   nvm install node
-  
+
   # set latest as default
   nvm alias default node
   nvm use default
-  
+
   echo "Node.js versions installed:"
   nvm list
   echo "Default version:"
